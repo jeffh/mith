6d4dbc2c328316e3c2832c37b1749cc51daf13fe
Jeff Hui <jeff@jeffhui.net>
2021-03-14 05:05:49 -0700

    Fix form expansion

--- src/net/jeffhui/mith.clj
+++ src/net/jeffhui/mith.clj
@@ -1,73 +1,69 @@
 (ns net.jeffhui.mith
   (:require [clojure.string :as string]))
 
-(declare html)
 (defn compile-html [form]
- (cond
-   (vector? form)
-   (let [sec (second form)
-         attrs (when (map? sec) sec)
-         children (if (map? sec)
-                    (rest (rest form))
-                    (rest form))]
-     `(net.jeffhui.mith/element ~(if (keyword? (first form))
-                                   (name (first form))
-                                   (first form))
-                                (clojure.core/clj->js ~(merge attrs
-                                                              {:class (string/join " " (let [cls (:class attrs)]
-                                                                                         (if (string? cls) [cls] cls)))}))
-                                (into-array ~(mapv (fn [f] (compile-html f)) children))))
-
-   (list? form)
-   (case (name (first form))
-     "if" `(if ~(second form) ~(compile-html (nth form 2)) ~(compile-html (nth form 3)))
-     "if-not" `(if-not ~(second form) ~(compile-html (nth form 2)) ~(compile-html (nth form 3)))
-     "if-some" `(if-some ~(second form) ~(compile-html (nth form 2)) ~(compile-html (nth form 3)))
-     "when" `(when ~@(butlast form) ~(compile-html (last form)))
-     "when-not" `(when-not ~@(butlast form) ~(compile-html (last form)))
-     "when-some" `(when-some ~@(butlast form) ~(compile-html (last form)))
-     "case" (let [[_ value & clauses] form]
-              `(case ~value
-                 ~@(mapcat (fn [[condition expr]]
-                             (if expr
-                               [condition (compile-html expr)]
-                               [(compile-html condition)]))
-                           (partition-all 2 clauses))))
-     "condp" (let [[_ f value & clauses] form]
-              `(condp ~f ~value
-                 ~@(mapcat (fn [[condition expr]]
-                             (if expr
-                               [condition (compile-html expr)]
-                               [(compile-html condition)]))
-                           (partition-all 2 clauses))))
-     "cond" (let [clauses (mapcat (fn [[condition expr]]
-                                   (if expr
-                                     [condition (compile-html expr)]
-                                     [(compile-html condition)]))
-                                 (partition-all 2 (rest form)))]
-             `(cond ~@clauses))
-     "let" `(let ~@(butlast form) ~(compile-html (last form)))
-     "let*" `(let* ~@(butlast form) ~(compile-html (last form)))
-     "letfn" `(letfn ~@(butlast form) ~(compile-html (last form)))
-     "letfn*" `(letfn* ~@(butlast form) ~(compile-html (last form)))
-     "for" `(for ~(second form) ~(compile-html (nth form 2)))
-     "do" (if (= 1 (count form))
-           (compile-html (first form))
-           `(do ~@(butlast form) ~(compile-html (last form))))
-     `(net.jeffhui.mith/interpret-element ~form))
-
-   (string? form) form
-   (number? form) form
-
-   :else `(net.jeffhui.mith/interpret-element ~form)))
-
-(defmacro html [form]
   (cond
     (vector? form)
-    (compile-html form)
+    (let [sec (second form)
+          attrs (when (map? sec) sec)
+          children (not-empty
+                    (if (map? sec)
+                      (rest (rest form))
+                      (rest form)))]
+      `(net.jeffhui.mith/element ~(if (keyword? (first form))
+                                    (name (first form))
+                                    (first form))
+                                 ~(when attrs
+                                    `(clojure.core/clj->js ~(merge attrs
+                                                                   (when (seq (:class attrs))
+                                                                     {:class (string/join " " (let [cls (:class attrs)]
+                                                                                                (if (string? cls) [cls] cls)))}))))
 
-    (keyword? form)
-    (name form)
+                                 ~(when children
+                                    `(into-array ~(mapv (fn [f] (compile-html f)) children)))))
 
-    :else
-    form))
+    (and (seq? form) (symbol? (first form)))
+    (case (name (first form))
+      "if" `(if ~(second form) ~(compile-html (nth form 2)) ~(compile-html (nth form 3)))
+      "if-not" `(if-not ~(second form) ~(compile-html (nth form 2)) ~(compile-html (nth form 3)))
+      "if-some" `(if-some ~(second form) ~(compile-html (nth form 2)) ~(compile-html (nth form 3)))
+      "when" `(when ~@(butlast (rest form)) ~(compile-html (last form)))
+      "when-not" `(when-not ~@(butlast (rest form)) ~(compile-html (last form)))
+      "when-some" `(when-some ~@(butlast (rest form)) ~(compile-html (last form)))
+      "case" (let [[_ value & clauses] form]
+               `(case ~value
+                  ~@(mapcat (fn [[condition expr]]
+                              (if expr
+                                [condition (compile-html expr)]
+                                [(compile-html condition)]))
+                            (partition-all 2 clauses))))
+      "condp" (let [[_ f value & clauses] form]
+                `(condp ~f ~value
+                   ~@(mapcat (fn [[condition expr]]
+                               (if expr
+                                 [condition (compile-html expr)]
+                                 [(compile-html condition)]))
+                             (partition-all 2 clauses))))
+      "cond" (let [clauses (mapcat (fn [[condition expr]]
+                                     (if expr
+                                       [condition (compile-html expr)]
+                                       [(compile-html condition)]))
+                                   (partition-all 2 (rest form)))]
+               `(cond ~@clauses))
+      "let" `(let ~@(butlast (rest form)) ~(compile-html (last form)))
+      "let*" `(let* ~@(butlast (rest form)) ~(compile-html (last form)))
+      "letfn" `(letfn ~@(butlast (rest form)) ~(compile-html (last form)))
+      "letfn*" `(letfn* ~@(butlast (rest form)) ~(compile-html (last form)))
+      "for" `(for ~(second form) ~(compile-html (nth form 2)))
+      "do" (if (= 1 (count form))
+             (compile-html (first form))
+             `(do ~@(butlast (rest form)) ~(compile-html (last form))))
+      `(net.jeffhui.mith/interpret-element ~form))
+
+    (string? form) form
+    (number? form) form
+
+    :else `(net.jeffhui.mith/interpret-element ~form)))
+
+(defmacro html [form]
+  (compile-html form))
---
+++ test/net/jeffhui/mith_test.clj
@@ -0,0 +1,187 @@
+(ns net.jeffhui.mith-test
+  (:require [net.jeffhui.mith :as m]
+            [clojure.test :refer [deftest are testing is]]))
+
+(def e `m/element)
+(def clj->js `clojure.core/clj->js)
+(def interp `m/interpret-element)
+
+(declare foo bar)
+
+(deftest test-compilation-of-html
+  (testing "Basic element transformations"
+    (are [input expected] (= (m/compile-html input) expected)
+      [:div] `(~e "div" nil nil)
+      [:div#id] `(~e "div#id" nil nil)
+      [:div#id.class1.class2] `(~e "div#id.class1.class2" nil nil)
+      [:div.class] `(~e "div.class" nil nil)
+
+      [:h1 "hello"] `(~e "h1" nil (into-array ["hello"]))
+      [:h1 "hello" "world"] `(~e "h1" nil (into-array ["hello" "world"]))
+      [:h1 {:class "foo"}] `(~e "h1" (~clj->js {:class "foo"}) nil)
+
+      [:p {:style "background: red"} "whoa"] `(~e "p" (~clj->js {:style "background: red"}) (into-array ["whoa"]))
+      [:p.c {:class ["a" "b"]} "whoa"] `(~e "p.c" (~clj->js {:class "a b"}) (into-array ["whoa"]))))
+
+  (testing "Nested element transformations"
+    (are [input expected] (= (m/compile-html input) expected)
+      [:div [:h1 "foo"]] `(~e "div" nil (into-array [(~e "h1" nil (into-array ["foo"]))]))
+      [:div [:h2 "foo"] [:h3 "bar"]] `(~e "div" nil (into-array [(~e "h2" nil (into-array ["foo"]))
+                                                                 (~e "h3" nil (into-array ["bar"]))]))))
+
+  (testing "Strings and numbers are left as is"
+    (is (= (m/compile-html "hello") "hello"))
+    (is (= (m/compile-html 1) 1)))
+
+  (testing "Unknown values are pass through an interpreter"
+    (is (= (m/compile-html {:foo :bar}) `(~interp {:foo :bar}))))
+
+  (testing "let form"
+    (are [input expected] (= (m/compile-html input) expected)
+      `(let [x (foo 2)] [:div x])
+      `(let [x (foo 2)] (~e "div" nil (into-array [(~interp x)])))))
+
+  (testing "let* form"
+    (are [input expected] (= (m/compile-html input) expected)
+      `(let* [x (foo 2)] [:div x])
+      `(let* [x (foo 2)] (~e "div" nil (into-array [(~interp x)])))))
+
+  (testing "letfn form"
+    (are [input expected] (= (m/compile-html input) expected)
+      `(letfn [x (foo [] 2)] [:div (x)])
+      `(letfn [x (foo [] 2)] (~e "div" nil (into-array [(~interp (x))])))))
+
+  (testing "letfn* form"
+    (are [input expected] (= (m/compile-html input) expected)
+      `(letfn* [x (foo [] 2)] [:div (x)])
+      `(letfn* [x (foo [] 2)] (~e "div" nil (into-array [(~interp (x))])))))
+
+  (testing "for form"
+    (are [input expected] (= (m/compile-html input) expected)
+      `(for [x (range 10)] [:div x])
+      `(for [x (range 10)] (~e "div" nil (into-array [(~interp x)])))))
+
+  (testing "do form"
+    (are [input expected] (= (m/compile-html input) expected)
+      `(do [:div foo] [:div bar])
+      `(do [:div foo] (~e "div" nil (into-array [(~interp bar)])))))
+
+  (testing "if form"
+    (are [input expected] (= (m/compile-html input) expected)
+      `(if (foo 2) [:div 1] [:div 2])
+      `(if (foo 2)
+         (~e "div" nil (into-array [1]))
+         (~e "div" nil (into-array [2])))
+
+      `(if (foo 2) [:div foo] [:div bar])
+      `(if (foo 2)
+         (~e "div" nil (into-array [(~interp foo)]))
+         (~e "div" nil (into-array [(~interp bar)])))))
+
+  (testing "if-not form"
+    (are [input expected] (= (m/compile-html input) expected)
+      `(if-not (foo 2) [:div 1] [:div 2])
+      `(if-not (foo 2)
+         (~e "div" nil (into-array [1]))
+         (~e "div" nil (into-array [2])))
+
+      `(if-not (foo 2) [:div foo] [:div bar])
+      `(if-not (foo 2)
+         (~e "div" nil (into-array [(~interp foo)]))
+         (~e "div" nil (into-array [(~interp bar)])))))
+
+  (testing "if-some form"
+    (are [input expected] (= (m/compile-html input) expected)
+      `(if-some (foo 2) [:div 1] [:div 2])
+      `(if-some (foo 2)
+         (~e "div" nil (into-array [1]))
+         (~e "div" nil (into-array [2])))
+
+      `(if-some (foo 2) [:div foo] [:div bar])
+      `(if-some (foo 2)
+         (~e "div" nil (into-array [(~interp foo)]))
+         (~e "div" nil (into-array [(~interp bar)])))))
+
+  (testing "when form"
+    (are [input expected] (= (m/compile-html input) expected)
+      `(when (foo 2) [:div 1] [:div 2])
+      `(when (foo 2)
+         [:div 1]
+         (~e "div" nil (into-array [2])))
+
+      `(when (foo 2) [:div foo] [:div bar])
+      `(when (foo 2)
+         [:div foo]
+         (~e "div" nil (into-array [(~interp bar)])))))
+
+  (testing "when-not form"
+    (are [input expected] (= (m/compile-html input) expected)
+      `(when-not (foo 2) [:div 1] [:div 2])
+      `(when-not (foo 2)
+         [:div 1]
+         (~e "div" nil (into-array [2])))
+
+      `(when-not (foo 2) [:div foo] [:div bar])
+      `(when-not (foo 2)
+         [:div foo]
+         (~e "div" nil (into-array [(~interp bar)])))))
+
+  (testing "when-some form"
+    (are [input expected] (= (m/compile-html input) expected)
+      `(when-some (foo 2) [:div 1] [:div 2])
+      `(when-some (foo 2)
+         [:div 1]
+         (~e "div" nil (into-array [2])))
+
+      `(when-some (foo 2) [:div foo] [:div bar])
+      `(when-some (foo 2)
+         [:div foo]
+         (~e "div" nil (into-array [(~interp bar)])))))
+
+  (testing "case form"
+    (are [input expected] (= (m/compile-html input) expected)
+      `(case (foo 2) 1 [:div 1] 2 [:div 2] [:div 3])
+      `(case (foo 2)
+         1 (~e "div" nil (into-array [1]))
+         2 (~e "div" nil (into-array [2]))
+         (~e "div" nil (into-array [3])))
+
+      `(case (foo 2) 1 [:div foo] 2 [:div bar] [:div 4])
+      `(case (foo 2)
+         1 (~e "div" nil (into-array [(~interp foo)]))
+         2 (~e "div" nil (into-array [(~interp bar)]))
+         (~e "div" nil (into-array [4])))))
+
+  (testing "condp form"
+    (are [input expected] (= (m/compile-html input) expected)
+      `(condp = (foo 2) 1 [:div 1] 2 [:div 2] [:div 3])
+      `(condp = (foo 2)
+         1 (~e "div" nil (into-array [1]))
+         2 (~e "div" nil (into-array [2]))
+         (~e "div" nil (into-array [3])))
+
+      `(condp = (foo 2) 1 [:div foo] 2 [:div bar] [:div 4])
+      `(condp = (foo 2)
+         1 (~e "div" nil (into-array [(~interp foo)]))
+         2 (~e "div" nil (into-array [(~interp bar)]))
+         (~e "div" nil (into-array [4])))))
+
+  (testing "cond form"
+    (are [input expected] (= (m/compile-html input) expected)
+      `(cond
+         (= (foo 2) 1) [:div 1]
+         (= (foo 2) 2) [:div 2]
+         :else [:div 3])
+      `(cond
+         (= (foo 2) 1) (~e "div" nil (into-array [1]))
+         (= (foo 2) 2) (~e "div" nil (into-array [2]))
+         :else (~e "div" nil (into-array [3])))
+
+      `(cond
+         (= (foo 2) 1) [:div foo]
+         (= (foo 2) 2) [:div bar]
+         :else [:div 4])
+      `(cond
+         (= (foo 2) 1) (~e "div" nil (into-array [(~interp foo)]))
+         (= (foo 2) 2) (~e "div" nil (into-array [(~interp bar)]))
+         :else (~e "div" nil (into-array [4]))))))
